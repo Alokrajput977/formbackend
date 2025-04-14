d4b220a6ceaadda6b79081ab0922ed541008d336
Alok Rajput <alokpaswan706@gmail.com>
2025-04-14 13:48:13 +0530

    $

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "http://localhost:3001",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true 
 }));
 
-// --- MongoDB Connection ---
-const mongoURI = process.env.MONGO_URI; // e.g., "mongodb://localhost:27017/mydb"
+
+const mongoURI = process.env.MONGO_URI; 
 
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
